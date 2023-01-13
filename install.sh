e2858a6ae5f134e07e4e701b9459a6ac9c39e874
Massimiliano Iannini <73445661+ThatsMassy@users.noreply.github.com>
2023-01-13 18:19:21 +0100

    fix

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ if [[ "$DISTRO" == *Fedora* ]];
         dnf -y update &&  dnf -y upgrade &&  dnf -y install python3-pygame python3-pip python3-qt5 python-tkinter tk python3-pillow-tk python3-pillow &&  pip3 install matplotlib &&  pip3 install pyqt5 && wget https://files.pythonhosted.org/packages/cc/74/11b04703ec416717b247d789103277269d567db575d2fd88f25d9767fe3d/pyserial-3.4.tar.gz && sudo tar -xvf pyserial-3.4.tar.gz && cd pyserial-3.4 &&  python3 setup.py install && cd .. && wget https://downloads.arduino.cc/arduino-1.8.19-linuxaarch64.tar.xz && sudo tar -xvf arduino-1.8.19-linuxaarch64.tar.xz && cd arduino-1.8.19 && sudo ./install.sh
      else
      dnf -y update &&  dnf -y upgrade &&  dnf -y install python3-pygame python3-pip python3-qt5 python-tkinter tk python3-pillow-tk python3-pillow &&  pip3 install matplotlib &&  pip3 install pyqt5 && wget https://files.pythonhosted.org/packages/cc/74/11b04703ec416717b247d789103277269d567db575d2fd88f25d9767fe3d/pyserial-3.4.tar.gz && sudo tar -xvf pyserial-3.4.tar.gz && cd pyserial-3.4 &&  python3 setup.py install && cd .. && wget https://downloads.arduino.cc/arduino-1.8.19-linux64.tar.xz && sudo tar -xvf arduino-1.8.19-linux64.tar.xz && cd arduino-1.8.19 && sudo ./install.sh
+     fi
   exit
 fi
 if [[ "$DISTRO" == *Ubuntu* ]];
@@ -32,6 +33,7 @@ if [[ "$DISTRO" == *Ubuntu* ]];
       apt update && apt -y install -y python3-pygame python3-matplotlib python3-pip &&  pip3 install matplotlib && wget https://files.pythonhosted.org/packages/cc/74/11b04703ec416717b247d789103277269d567db575d2fd88f25d9767fe3d/pyserial-3.4.tar.gz && sudo tar -xvf pyserial-3.4.tar.gz && cd pyserial-3.4 && sudo python3 setup.py install && cd .. && wget https://downloads.arduino.cc/arduino-1.8.19-linuxaarch64.tar.xz &&  tar -xvf arduino-1.8.19-linuxaarch64.tar.xz && cd arduino-1.8.19 && sudo ./install.sh
     else
       apt update && apt -y install -y python3-pygame python3-matplotlib python3-pip &&  pip3 install matplotlib && wget https://files.pythonhosted.org/packages/cc/74/11b04703ec416717b247d789103277269d567db575d2fd88f25d9767fe3d/pyserial-3.4.tar.gz && sudo tar -xvf pyserial-3.4.tar.gz && cd pyserial-3.4 && sudo python3 setup.py install && cd .. && wget https://downloads.arduino.cc/arduino-1.8.19-linux64.tar.xz &&  tar -xvf arduino-1.8.19-linux64.tar.xz && cd arduino-1.8.19 && sudo ./install.sh
+     fi
   exit
 fi
 if [[ "$DISTRO" == *Arch* ]];
@@ -43,6 +45,7 @@ if [[ "$DISTRO" == *Arch* ]];
       sudo pacman -Syyu && sudo pacman -S tk python-pygame python-pip && sudo pip install matplotlib && wget https://files.pythonhosted.org/packages/cc/74/11b04703ec416717b247d789103277269d567db575d2fd88f25d9767fe3d/pyserial-3.4.tar.gz && sudo tar -xvf pyserial-3.4.tar.gz && cd pyserial-3.4 && sudo python setup.py install && cd .. && wget https://downloads.arduino.cc/arduino-1.8.19-linuxaarch64.tar.xz && sudo tar -xvf arduino-1.8.19-linuxaarch64.tar.xz && cd arduino-1.8.19 && sudo ./install.sh
       else
       sudo pacman -Syyu && sudo pacman -S tk python-pygame python-pip && sudo pip install matplotlib && wget https://files.pythonhosted.org/packages/cc/74/11b04703ec416717b247d789103277269d567db575d2fd88f25d9767fe3d/pyserial-3.4.tar.gz && sudo tar -xvf pyserial-3.4.tar.gz && cd pyserial-3.4 && sudo python setup.py install && cd .. && wget https://downloads.arduino.cc/arduino-1.8.19-linux64.tar.xz && sudo tar -xvf arduino-1.8.19-linux64.tar.xz && cd arduino-1.8.19 && sudo ./install.sh
+      fi
   exit
 fi
 if [[ "$DISTRO" == *openSUSE* ]];
@@ -54,5 +57,6 @@ if [[ "$DISTRO" == *openSUSE* ]];
     zypper ref && zypper up && zypper in python3-pygame python3-matplotlib python3-pip && sudo pip3 install matplotlib && wget https://files.pythonhosted.org/packages/cc/74/11b04703ec416717b247d789103277269d567db575d2fd88f25d9767fe3d/pyserial-3.4.tar.gz && sudo tar -xvf pyserial-3.4.tar.gz && cd pyserial-3.4 && sudo python3 setup.py install && cd .. && wget https://downloads.arduino.cc/arduino-1.8.19-linuxaarch64.tar.xz && sudo tar -xvf arduino-1.8.19-linuxaarch64.tar.xz && cd arduino-1.8.19 && sudo ./install.sh
     else
     zypper ref && zypper up && zypper in python3-pygame python3-matplotlib python3-pip && sudo pip3 install matplotlib && wget https://files.pythonhosted.org/packages/cc/74/11b04703ec416717b247d789103277269d567db575d2fd88f25d9767fe3d/pyserial-3.4.tar.gz && sudo tar -xvf pyserial-3.4.tar.gz && cd pyserial-3.4 && sudo python3 setup.py install && cd .. && wget https://downloads.arduino.cc/arduino-1.8.19-linux64.tar.xz && sudo tar -xvf arduino-1.8.19-linux64.tar.xz && cd arduino-1.8.19 && sudo ./install.sh
+    fi
   exit
 fi

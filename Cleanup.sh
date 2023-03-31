d6b3081beb136c54b8656fdeba5583d6b3267746
Massimiliano Iannini <73445661+ThatsMassy@users.noreply.github.com>
2023-03-31 08:46:42 +0200

    Cleanup.sh 1.2
    fixed a typo

--- Cleanup.sh
+++ Cleanup.sh
@@ -1,5 +1,5 @@
 AARCH=$(hostnamectl | grep -i "architecture")
-if [[ $AARCH == *x86-64* ]];
+if [[ "$AARCH" == *x86-64* ]];
 	then
 		sudo rm -rf arduino-1.8.19-linux64.tar.xz && sudo rm -rf pyserial-3.4.tar.gz
 		else
